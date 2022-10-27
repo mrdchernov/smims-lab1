a08d9954a0044799401eb8f4c99c5773e5bb258b
Dmytro Chernov <chernov@Dmytros-MacBook-Pro.local>
2022-10-27 22:43:52 +0300

    pg error

--- next.config.js
+++ next.config.js
@@ -1,19 +1,16 @@
 /** @type {import('next').NextConfig} */
 
-const nodeExternals = require('webpack-node-externals');
+// const nodeExternals = require('webpack-node-externals');
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: async (config) => {
-    console.log(config.externals)
-    if (config.externals) {
-      config.externals.push(
-        await nodeExternals()
-      );
-    } else {
-      config.externals = [ await nodeExternals() ];
-    }
+  webpack: (config) => {
+    // if (config.externals) {
+    //   config.externals.push(nodeExternals());
+    // } else {
+    //   config.externals = [ nodeExternals() ];
+    // }
     return config;
 
   }
