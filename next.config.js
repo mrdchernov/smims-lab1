03ad7605b6e1718becbf9c962878e0fa2df7830a
Dmytro Chernov <chernov@Dmytros-MacBook-Pro.local>
2022-10-27 22:29:24 +0300

    added externals to config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+const nodeExternals = require('webpack-node-externals');
+
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  webpack: async (config) => {
+    console.log(config.externals)
+    if (config.externals) {
+      config.externals.push(
+        await nodeExternals()
+      );
+    } else {
+      config.externals = [ await nodeExternals() ];
+    }
+    return config;
+
+  }
 }
 
 
