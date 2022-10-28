713b9285b4b58b2b5c5495ce8d489bf7ea02e085
Dmytro Chernov <chernov@Dmytros-MacBook-Pro.local>
2022-10-28 10:59:48 +0300

    added standalone support

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-// const nodeExternals = require('webpack-node-externals');
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'standalone',
   webpack: (config) => {
-    // if (config.externals) {
-    //   config.externals.push(nodeExternals());
-    // } else {
-    //   config.externals = [ nodeExternals() ];
-    // }
     return config;
 
   }
