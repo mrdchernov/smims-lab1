3b29610065529f02d8615d913426c4fd28febd8b
Dmytro Chernov <chernov@Dmytros-MacBook-Pro.local>
2022-11-03 11:18:38 +0200

    moved server

--- config/index.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    POSTGRES_USER: process.env.POSTGRES_USER,
-    POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
-    POSTGRES_DB: process.env.POSTGRES_DB,
-    POSTGRES_PORT: process.env.POSTGRES_PORT || 5432,
-    POSTGRES_HOST: process.env.POSTGRES_HOST
-};
